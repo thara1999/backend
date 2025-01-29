c80f4de4e74341840b3f9c08586b16cc20998677
thara1999 <tamilkrishnan20@gmail.com>
2025-01-29 12:32:54 +0530

    9

--- bcript/user.js
+++ bcript/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 
 const userRouter = express.Router();
